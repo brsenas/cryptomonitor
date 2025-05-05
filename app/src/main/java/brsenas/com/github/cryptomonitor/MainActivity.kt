b2c2911c75b1e23f7381f6e5ecd01dbf22787963
Bruno Sena <brunosen4s@gmail.com>
2025-05-05 19:55:22 -0300

    fix: adjustments to make the code run correctly

--- app/src/main/java/brsenas/com/github/cryptomonitor/MainActivity.kt
+++ app/src/main/java/brsenas/com/github/cryptomonitor/MainActivity.kt
@@ -15,7 +15,6 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
